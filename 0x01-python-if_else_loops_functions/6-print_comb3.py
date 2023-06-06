cdd726d8b66db598a60b776a8e4b6971ebc96b3c
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-06-07 01:40:35 +0300

    hot fix 02

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for i in range(1, 90):
     if i % 10 == 0:
         i += i // 10 + 1
         continue
-    print(f"{i:02d}", end=", " if i < 89 else "\n")
+    print("{:02d}".format(i), end=", " if i < 89 else "\n")

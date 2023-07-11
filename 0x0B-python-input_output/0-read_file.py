c28cee56f5e44b2a7631a596f38d518a635892cb
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-07-11 22:11:42 +0300

    hot fix 01

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     """
-    Read the contents of a text file and print it.
+    Read the contents of a text file (UTF8) and print it to stdout.
 
     Args:
         filename (str): The name of the file to read. If not specified,
                         an empty string is used.
 
     """
-    with open(filename, "r", encoding='utf-8') as file:
-        print(file.read(), end="")
+    with open(filename, "r", encoding="utf-8") as file:
+        for line in file:
+            print(line, end="")

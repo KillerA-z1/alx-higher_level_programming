8701eb3c7867242dd49095af6bb428ca6f4d2554
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-07-12 18:09:04 +0300

    hot fix 05

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,4 +1,7 @@
-#include "Python.h"
+#include <Python.h>
+#include <stdio.h>
+
+void print_python_string(PyObject *p);
 
 /**
  * print_python_string - Print information about a Python string object
@@ -6,7 +9,6 @@
  */
 void print_python_string(PyObject *p)
 {
-	PyUnicodeObject *str_obj = (PyUnicodeObject *) p;
 	Py_ssize_t length;
 	wchar_t *wide_str;
 
@@ -17,15 +19,15 @@ void print_python_string(PyObject *p)
 		return;
 	}
 
-	length = PyUnicode_GET_LENGTH(str_obj);
-	wide_str = PyUnicode_AsWideCharString(str_obj, &length);
+	length = PyUnicode_GET_LENGTH(p);
+	wide_str = PyUnicode_AsWideCharString(p, &length);
 	if (wide_str == NULL)
 	{
 		printf("  [ERROR] Failed to get wide char string\n");
 		return;
 	}
 
-	printf("  type: %s\n", PyUnicode_IS_COMPACT_ASCII(str_obj) ? "compact ascii" : "compact unicode object");
+	printf("  type: %s\n", PyUnicode_IS_COMPACT_ASCII(p) ? "compact ascii" : "compact unicode object");
 	printf("  length: %ld\n", length);
 	printf("  value: %ls\n", wide_str);
 

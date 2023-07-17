c8f9d667feaa378bb05b5ba325ab4e8075fd58cb
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-07-17 16:46:43 +0300

    change function def update(self, *args) to update(self, *args, **kwargs) that assigns a key/value argument to attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -148,10 +148,14 @@ class Rectangle(Base):
             self.id, self.__x, self.__y, self.__width, self.__height)
         return result
 
-    def update(self, *args):
-        """Update attributes"""
-        self.id = args[0] if len(args) >= 1 else self.id
-        self.width = args[1] if len(args) >= 2 else self.width
-        self.height = args[2] if len(args) >= 3 else self.height
-        self.x = args[3] if len(args) >= 4 else self.x
-        self.y = args[4] if len(args) >= 5 else self.y
+    def update(self, *args, **kwargs):
+        """Updating data"""
+        attributes = ['id', 'width', 'height', 'x', 'y']
+    
+        if args:
+            for i, arg in enumerate(args):
+                setattr(self, attributes[i], arg)
+        else:
+            for key in kwargs:
+                if key in attributes:
+                    setattr(self, key, kwargs[key])

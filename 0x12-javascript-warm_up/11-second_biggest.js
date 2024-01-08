c3b8b2751cdff911a6a11a1d8a37c320bec31c29
KillerA-z1 <Ahmedz1a2m@gmail.com>
2024-01-09 01:46:58 +0300

    script that searches the second biggest integer in the list of arguments.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
+
+// script that searches the second biggest integer in the list of arguments.
+
 const args = process.argv.slice(2).map(Number).sort((a, b) => b - a);
 
 if (args.length <= 1) {

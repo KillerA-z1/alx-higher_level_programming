5a3496902bbc3a36f27ecec0ad036388902b68b7
KillerA-z1 <ahmedz1a2m@gmail.com>
2024-06-23 23:37:34 +0300

    Hot Fix 02

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,8 +8,8 @@ from sys import argv
 
 if __name__ == "__main__":
     # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", user=argv[1],
-                         passwd=argv[2], db=argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], )
     cur = db.cursor()
 
     # Execute the SQL query
@@ -20,9 +20,7 @@ if __name__ == "__main__":
     rows = cur.fetchall()
 
     # Print the results
-    for row in rows:
-        print(row)
-
+    [print(rows) for row in rows if row[1][0] == "N"]
     # Close the cursor and the database connection
     cur.close()
     db.close()

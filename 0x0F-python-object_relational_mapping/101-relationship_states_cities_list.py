13ef8c6b63c34467e1280a48ab244253e34038be
KillerA-z1 <ahmedz1a2m@gmail.com>
2024-06-28 18:07:20 +0300

    Hot Fix 03

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 """
 This script lists all State objects and their corresponding City objects.
-
-The script takes three command-line arguments: username, password, and dbname.
-It connects to a MySQL database using the provided credentials and retrieves
-all State objects from the database. For each State object, it prints the state
-ID and name. It then retrieves all City objects associated with the state and
-prints their IDs and names as well.
-
 """
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -17,9 +11,9 @@ from model_state import Base, State, City
 
 if __name__ == "__main__":
     username, password, dbname = sys.argv[1], sys.argv[2], sys.argv[3]
-    engine = create_engine(
-        f'mysql+mysqldb://{username}:{password}@localhost/{dbname}',
-        pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, dbname),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
